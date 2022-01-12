f127754aacd2fe40dfd99c5bb82b8f2e31b78beb
Shehzaan Mansuri <shehzaanmansuri1@gmail.com>
2022-01-12 23:28:45 +0530

    Material App

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Home Route'),
+            ),
+          );
+        },
+        '/about': (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('About Route'),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
